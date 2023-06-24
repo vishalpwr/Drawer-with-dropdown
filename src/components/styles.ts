2a5fa646d50820e3879c1dca822eaebaa5b19911
Vishalpwr <vishalvir8p@gmail.com>
2023-06-24 13:39:06 +0530

    Add comp. styles

---
+++ src/components/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import Styles from "../common/Styles";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  row: {
+    ...Styles.rowView,
+  },
+})
